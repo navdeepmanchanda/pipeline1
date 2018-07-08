ca9cc106d936843fd5dc6a06418da5d5b1364416
navdeep <root@localhost.localdomain>
2018-07-08 09:42:30 +0000

    added code compilation logic

---
+++ src/com/opstree/devops/build/maven/compile.groovy
@@ -0,0 +1,20 @@
+package com.opstree.devops.build.maven
+
+/***************************************
+***** function to compile the code *****
+***************************************/
+def code_compile(String maven_goals) {
+   wrap([$class: 'AnsiColorBuildWrapper']) {
+        println "\u001B[32m[INFO] => compiling java code, please wait..."
+        sh "mvn ${maven_goals}"
+      }
+   }
+   catch(Exception e)
+   {
+      wrap([$class: 'AnsiColorBuildWrapper']) {
+         print "\u001B[41m[ERROR] => failed to compile the code, exiting..."
+         currentBuild.result = "FAILURE"
+         throw e
+      }
+   }
+}
