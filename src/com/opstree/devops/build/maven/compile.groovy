2f977e367e0932407623481eb7db5c88d5684c3c
navdeep <root@localhost.localdomain>
2018-07-08 10:56:46 +0000

    added junit dependency

--- src/com/opstree/devops/build/maven/compile.groovy
+++ src/com/opstree/devops/build/maven/compile.groovy
@@ -8,9 +8,11 @@ def code_compile(String maven_goals, String pom_path) {
       wrap([$class: 'AnsiColorBuildWrapper']) {
         println "\u001B[32m[INFO] => compiling java code, please wait..."
         def maven_home = tool name: 'MAVEN_HOME', type: 'maven'
+        def java_home  = tool name: 'JAVA_HOME', type: 'jdk'
         MAVEN_GOALS = maven_goals
         POM_PATH = pom_path
-        env.PATH = "${maven_home}/bin:${env.PATH}"
+        env.JAVA_HOME = java_home
+        env.PATH = "${maven_home}/bin:${java_home{/bin:${env.PATH}"
         sh "mvn -f ${POM_PATH} ${MAVEN_GOALS}"
       }
    }
@@ -30,9 +32,11 @@ def unit_tests(String maven_test_goals, String pom_path) {
       wrap([$class: 'AnsiColorBuildWrapper']) {
         println "\u001B[32m[INFO] => compiling java code, please wait..."
         def maven_home = tool name: 'MAVEN_HOME', type: 'maven'
+        def java_home  = tool name: 'JAVA_HOME', type: 'jdk'
+        env.JAVA_HOME = java_home
         MAVEN_TEST_GOALS = maven_test_goals
         POM_PATH = pom_path
-        env.PATH = "${maven_home}/bin:${env.PATH}"
+        env.PATH = "${maven_home}/bin:${JAVA_HOME/bin:${env.PATH}"
         sh "mvn -f ${POM_PATH} ${MAVEN_TEST_GOALS}"
       }
    }
